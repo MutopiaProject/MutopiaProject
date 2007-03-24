2ea4e53764c0b6c637f420f36bc2b45294c195cb
Chris Sawer <chris@mutopiaproject.org>
2007-03-24 10:49:17 +0000

    Add RDF capabilities to mutopia-combine.sh

--- UsefulScripts/mutopia-combine.sh
+++ UsefulScripts/mutopia-combine.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
 
-if [ ! -n "$1" ] ; then
-    echo "Usage: $0 <base name>"
+if [ ! -n "$2" ] ; then
+    echo "Usage: $0 <base name> <header file>"
     exit 1
 fi
 
-if [ `ls *-a4.pdf | wc -l` -gt 1 ] ; then
-    gunzip *-a4.ps.gz
-    gunzip *-let.ps.gz
-    zip "$1"-a4-pdfs.zip *-a4.pdf
-    zip "$1"-a4-pss.zip *-a4.ps
-    zip "$1"-let-pdfs.zip *-let.pdf
-    zip "$1"-let-pss.zip *-let.ps
-    rm *-a4.pdf *-a4.ps *-let.pdf *-let.ps
+BASE_NAME=$1
+HEADER_FILE=$2
+LY_COUNT=`ls *.ly *.lyi *.ily | wc -l`
+PDF_COUNT=`ls *-a4.pdf | wc -l`
+MID_COUNT=`ls *.mid | wc -l`
+
+# Create zip files as appropriate
+if [ $PDF_COUNT -gt 1 ] ; then
+   gunzip *-a4.ps.gz
+   gunzip *-let.ps.gz
+   zip "$BASE_NAME"-a4-pdfs.zip *-a4.pdf
+   zip "$BASE_NAME"-a4-pss.zip *-a4.ps
+   zip "$BASE_NAME"-let-pdfs.zip *-let.pdf
+   zip "$BASE_NAME"-let-pss.zip *-let.ps
+   rm *-a4.pdf *-a4.ps *-let.pdf *-let.ps
 fi
 
-if [ `ls *.ly *.lyi *.ily | wc -l` -gt 1 ] ; then
-    zip "$1"-lys.zip *.ly *.lyi *.ily
-    rm *.ly *.lyi *.ily
+if [ $LY_COUNT -gt 1 ] ; then
+   # Backup header file
+   cp "$HEADER_FILE" "$BASE_NAME".ly2
+
+   zip "$BASE_NAME"-lys.zip *.ly *.lyi *.ily
+   rm *.ly *.lyi *.ily
+
+   # Rename header file
+   mv "$BASE_NAME".ly2 "$BASE_NAME".ly
 fi
 
-if [ `ls *.mid | wc -l` -gt 1 ] ; then
-    zip "$1"-mids.zip *.mid
-    rm *.mid
+if [ $MID_COUNT -gt 1 ] ; then
+   zip "$BASE_NAME"-mids.zip *.mid
+   rm *.mid
+fi
+
+# Create RDF
+java Mutopia -r "$BASE_NAME".ly
+
+# Remove header file if appropriate
+if [ $LY_COUNT -gt 1 ] ; then
+   rm "$BASE_NAME".ly
 fi
 
