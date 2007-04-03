a2bc3aedcb3970ce9a2fa0a5d3a19eea3936c610
Chris Sawer <chris@mutopiaproject.org>
2007-04-03 16:50:30 +0000

    Redo list of files classified as "ly" in combining script

--- UsefulScripts/mutopia-combine.sh
+++ UsefulScripts/mutopia-combine.sh
@@ -7,7 +7,8 @@ fi
 
 BASE_NAME=$1
 HEADER_FILE=$2
-LY_COUNT=`ls *.ly *.lyi *.ily | wc -l`
+LY_FILES="*.ly *.lyi *.ily *.txt"
+LY_COUNT=`ls $LY_FILES | wc -l`
 PDF_COUNT=`ls *-a4.pdf | wc -l`
 MID_COUNT=`ls *.mid | wc -l`
 
@@ -26,8 +27,8 @@ if [ $LY_COUNT -gt 1 ] ; then
    # Backup header file
    cp "$HEADER_FILE" "$BASE_NAME".ly2
 
-   zip "$BASE_NAME"-lys.zip *.ly *.lyi *.ily
-   rm *.ly *.lyi *.ily
+   zip "$BASE_NAME"-lys.zip $LY_FILES
+   rm $LY_FILES
 
    # Rename header file
    mv "$BASE_NAME".ly2 "$BASE_NAME".ly
