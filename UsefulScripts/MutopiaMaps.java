fa9f68161a39c68aa7915c46d5ea97c18353b09e
Chris Sawer <chris@mutopiaproject.org>
2007-04-29 10:41:19 +0000

    Add composer and style maps (read from standard data files)

--- UsefulScripts/MutopiaMaps.java
+++ UsefulScripts/MutopiaMaps.java
@@ -1,12 +1,27 @@
+/*
+ * Filename:         MutopiaMaps.java
+ * Original author:  Chris Sawer
+ * Subversion:       $Revision:$
+ * Last changed:     $Date:$
+ *
+ * Description:
+ *   Class to set up licence, composer and style maps.
+ */
+
+import java.io.*;
 import java.util.*;
 
 public class MutopiaMaps
 {
    public static Map<String, String> licenceMap = new HashMap<String, String>();
+   public static Map<String, String> composerMap = new HashMap<String, String>();
+   public static Map<String, String> styleMap = new HashMap<String, String>();
    
    static
    {
       initialiseLicenceMap();
+      readDatFileIntoMap("composers.dat", composerMap);
+      readDatFileIntoMap("styles.dat", styleMap);
    }
    
    private static void initialiseLicenceMap()
@@ -37,5 +52,52 @@ public class MutopiaMaps
                      "\\hspace #-1.0 \\teeny .org } by \\maintainer \\hspace #-1.0 . \\hspace #0.5 Copyright \u00A9 " + now.get(Calendar.YEAR) + ". \\hspace " +
                      "#0.5 Reference: \\footer } } \\line { \\teeny \\line { Licensed under the Creative Commons Attribution-ShareAlike 3.0 (Unported) License, for details " +
                      "see: \\hspace #-0.5 \\with-url #\"http://creativecommons.org/licenses/by-sa/3.0\" http://creativecommons.org/licenses/by-sa/3.0 } } } }");
+   }
+
+   private static void readDatFileIntoMap(String datFile, Map<String, String> map)
+   {
+      // Get directory
+      String mutopiaDir = System.getenv("MUTOPIA_BASE");
+      if (mutopiaDir == null)
+      {
+         throw new RuntimeException("MUTOPIA_BASE not set");
+      }
+      String filename = mutopiaDir + "/datafiles/" + datFile;
+
+      try
+      {
+         // Open file
+         BufferedReader dataFile = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"));
+
+         // Read key/value pairs
+         String key = dataFile.readLine();
+         String value = dataFile.readLine();
+         do
+         {
+            // Check value and put into map
+            if (value != null)
+            {
+               map.put(key, value);
+            }
+            else
+            {
+               System.err.println("Inconsistency in data file: " + filename);
+            }
+
+            key = dataFile.readLine();
+            value = dataFile.readLine();
+         }
+         while (key != null); // key == null at end of file
+
+         dataFile.close();
+      }
+      catch (FileNotFoundException ex)
+      {  
+         System.err.println("Data file " + filename + " not found");
+      }
+      catch (IOException ex)
+      {  
+         System.err.println(ex);
+      }
    }
 }
