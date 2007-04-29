b773a0a1606a998996e134f3da59f91d7c517f0d
Chris Sawer <chris@mutopiaproject.org>
2007-04-29 09:27:46 +0000

    Add MUTOPIA_BASE and usage details

--- UsefulScripts/mutopia-env.sh
+++ UsefulScripts/mutopia-env.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# Filename:         mutopia-env.sh
+# Original author:  Chris Sawer
+# Subversion:       $Revision$
+# Last changed:     $Date$
+#
+# Description:
+#   Script to set up the appropriate LilyPond and Mutopia environment variables
+#   for Mutopia compilation.
+#   It should be sourced rather than run.
+#
+# Options:
+#   $1 - (optional) LilyPond version to use
+
 # Can specify a specific version on the command line
-if [ "$1" = "-v" ]; then
-   LILYPOND_VERSION=$2
+if [ -n "$1" ]; then
+   LILYPOND_VERSION=$1
    LILYPOND_BASE=/Applications/LilyPond/$LILYPOND_VERSION
 else
    LILYPOND_VERSION=
@@ -13,3 +26,4 @@ export LILYPOND_VERSION
 export LILYPOND_BASE
 export LILYPOND_BIN=$LILYPOND_BASE/LilyPond.app/Contents/Resources/bin/lilypond
 export CONVERT_LY_BIN=$LILYPOND_BASE/LilyPond.app/Contents/Resources/bin/convert-ly
+export MUTOPIA_BASE=~/Mutopia
