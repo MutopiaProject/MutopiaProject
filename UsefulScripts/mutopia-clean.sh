0be6045986f3112638e3ce5f00731afcb1355013
Chris Sawer <chris@mutopiaproject.org>
2007-05-20 10:38:27 +0000

    Add mutopia-clean.sh

---
+++ UsefulScripts/mutopia-clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Filename:         mutopia-clean.sh
+# Original author:  Chris Sawer
+# Subversion:       $Revision $
+# Last changed:     $Date $
+#
+# Description:
+#   (Tiny) script to remove stale .mid/.png/.pdf/.ps.gz/.rdf files for Mutopia.
+
+rm -f *.png *.mid *-mids.zip *.pdf *-pdfs.zip *.ps.gz *-pss.zip *-lys.zip *.rdf
+
