538c71dad9c866d69db6e7542adfd15d3297f420
Chris Sawer <chris@mutopiaproject.org>
2008-03-11 21:37:50 +0000

    Add *.log to list of files to clean

--- UsefulScripts/mutopia-clean.sh
+++ UsefulScripts/mutopia-clean.sh
@@ -8,5 +8,5 @@
 # Description:
 #   Script to remove stale .zip/.png/.mid/.pdf/.ps.gz/.rdf files for Mutopia.
 
-rm -f *.png *.mid *-mids.zip *.pdf *-pdfs.zip *.ps.gz *-pss.zip *-lys.zip *.rdf
+rm -f *.png *.mid *-mids.zip *.pdf *-pdfs.zip *.ps.gz *-pss.zip *-lys.zip *.rdf *.log
 
