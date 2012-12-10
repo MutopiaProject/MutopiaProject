3b038f05e12710c2a2915ad8187edf60572472e8
Javier Ruiz-Alma <javier@ruiz-alma.com>
2012-12-09 17:32:23 -0800

    Update tagline scripts for Lylipond 2.16.1. Close #6

--- UsefulScripts/MutopiaMaps.java
+++ UsefulScripts/MutopiaMaps.java
@@ -80,28 +80,28 @@ public class MutopiaMaps
       
       licenceMapNew.put("Public Domain",
                      "tagline = \\markup { \\override #'(box-padding . 1.0) \\override #'(baseline-skip . 2.7) \\box \\center-column { \\small \\line { " +
-                     "Sheet music from \\with-url #\"http://www.MutopiaProject.org\" \\line { \\teeny www. \\hspace #-0.5 MutopiaProject \\hspace #-0.5 " +
-                     "\\teeny .org \\hspace #0.5 } \u2022 \\hspace #0.5 \\italic Free to download, with the \\italic freedom to distribute, modify and perform. " +
-                     "} \\line { \\small \\line { Typeset using \\with-url #\"http://www.LilyPond.org\" \\line { \\teeny www. \\hspace #-0.5 LilyPond " +
-                     "\\hspace #-0.5 \\teeny .org } by \\maintainer \\hspace #-0.6 . \\hspace #0.5 Reference: \\footer } } \\line { \\teeny \\line { This sheet " +
-                     "music has been placed in the public domain by the typesetter, for details see: \\hspace #-0.5 \\with-url " +
-                     "#\"http://creativecommons.org/licenses/publicdomain\" http://creativecommons.org/licenses/publicdomain } } } }");
+                     "Sheet music from \\with-url #\"http://www.MutopiaProject.org\" \\line { \\concat { \\teeny www. \\normalsize MutopiaProject " +
+                     "\\teeny .org } \\hspace #0.5 } \u2022 \\hspace #0.5 \\italic Free to download, with the \\italic freedom to distribute, modify and perform. " +
+                     "} \\line { \\small \\line { Typeset using \\with-url #\"http://www.LilyPond.org\" \\line { \\concat { \\teeny www. \\normalsize LilyPond " +
+                     "\\teeny .org }} by \\concat { \\maintainer . } \\hspace #0.5 Reference: \\footer } } \\line { \\teeny \\line { This sheet " +
+                     "music has been placed in the public domain by the typesetter, for details " +
+      		     "\\concat { see: \\hspace #0.3 \\with-url #\"http://creativecommons.org/licenses/publicdomain\" http://creativecommons.org/licenses/publicdomain } } } } }");
       licenceMapNew.put("Creative Commons Attribution 3.0",
                      "tagline = \\markup { \\override #'(box-padding . 1.0) \\override #'(baseline-skip . 2.7) \\box \\center-column { \\small \\line { " +
-                     "Sheet music from \\with-url #\"http://www.MutopiaProject.org\" \\line { \\teeny www. \\hspace #-0.5 MutopiaProject \\hspace #-0.5 " +
-                     "\\teeny .org \\hspace #0.5 } \u2022 \\hspace #0.5 \\italic Free to download, with the \\italic freedom to distribute, modify and perform. " +
-                     "} \\line { \\small \\line { Typeset using \\with-url #\"http://www.LilyPond.org\" \\line { \\teeny www. \\hspace #-0.5 LilyPond " +
-                     "\\hspace #-0.5 \\teeny .org } by \\maintainer \\hspace #-0.6 . \\hspace #0.5 Copyright \u00A9 " + now.get(Calendar.YEAR) + ". \\hspace " +
-                     "#0.5 Reference: \\footer } } \\line { \\teeny \\line { Licensed under the Creative Commons Attribution 3.0 (Unported) License, for details see: " +
-                     "\\hspace #-0.5 \\with-url #\"http://creativecommons.org/licenses/by/3.0\" http://creativecommons.org/licenses/by/3.0 } } } }");
+                     "Sheet music from \\with-url #\"http://www.MutopiaProject.org\" \\line { \\concat { \\teeny www. \\normalsize MutopiaProject " +
+                     "\\teeny .org } \\hspace #0.5 } \u2022 \\hspace #0.5 \\italic Free to download, with the \\italic freedom to distribute, modify and perform. " +
+                     "} \\line { \\small \\line { Typeset using \\with-url #\"http://www.LilyPond.org\" \\line { \\concat { \\teeny www. \\normalsize LilyPond " +
+                     "\\teeny .org }} by \\concat { \\maintainer . } \\hspace #0.5 Copyright \u00A9 " + now.get(Calendar.YEAR) + ". \\hspace " +
+                     "#0.5 Reference: \\footer } } \\line { \\teeny \\line { Licensed under the Creative Commons Attribution 3.0 (Unported) License, for details " +
+      		     "\\concat { see: \\hspace #0.3 \\with-url #\"http://creativecommons.org/licenses/by/3.0\" http://creativecommons.org/licenses/by/3.0 } } } } }");      
       licenceMapNew.put("Creative Commons Attribution-ShareAlike 3.0",
                      "tagline = \\markup { \\override #'(box-padding . 1.0) \\override #'(baseline-skip . 2.7) \\box \\center-column { \\small \\line { " +
-                     "Sheet music from \\with-url #\"http://www.MutopiaProject.org\" \\line { \\teeny www. \\hspace #-0.5 MutopiaProject \\hspace #-0.5 " +
-                     "\\teeny .org \\hspace #0.5 } \u2022 \\hspace #0.5 \\italic Free to download, with the \\italic freedom to distribute, modify and perform. " +
-                     "} \\line { \\small \\line { Typeset using \\with-url #\"http://www.LilyPond.org\" \\line { \\teeny www. \\hspace #-0.5 LilyPond " +
-                     "\\hspace #-0.5 \\teeny .org } by \\maintainer \\hspace #-0.6 . \\hspace #0.5 Copyright \u00A9 " + now.get(Calendar.YEAR) + ". \\hspace " +
+                     "Sheet music from \\with-url #\"http://www.MutopiaProject.org\" \\line { \\concat { \\teeny www. \\normalsize MutopiaProject " +
+                     "\\teeny .org } \\hspace #0.5 } \u2022 \\hspace #0.5 \\italic Free to download, with the \\italic freedom to distribute, modify and perform. " +
+                     "} \\line { \\small \\line { Typeset using \\with-url #\"http://www.LilyPond.org\" \\line { \\concat { \\teeny www. \\normalsize LilyPond " +
+                     "\\teeny .org }} by \\concat { \\maintainer . } \\hspace #0.5 Copyright \u00A9 " + now.get(Calendar.YEAR) + ". \\hspace " +
                      "#0.5 Reference: \\footer } } \\line { \\teeny \\line { Licensed under the Creative Commons Attribution-ShareAlike 3.0 (Unported) License, for details " +
-                     "see: \\hspace #-0.5 \\with-url #\"http://creativecommons.org/licenses/by-sa/3.0\" http://creativecommons.org/licenses/by-sa/3.0 } } } }");
+                     "\\concat { see: \\hspace #0.3 \\with-url #\"http://creativecommons.org/licenses/by-sa/3.0\" http://creativecommons.org/licenses/by-sa/3.0 } } } } }");
    }
 
    private static void readDatFileIntoMap(String datFile, Map<String, String> map)
