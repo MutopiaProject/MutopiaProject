636a59cf3cac7557fee30c99d4a1639a0c90d865
Chris Sawer <chris@mutopiaproject.org>
2015-11-10 11:58:17 +0000

    Update mutopia-env for build server

--- UsefulScripts/mutopia-env.sh
+++ UsefulScripts/mutopia-env.sh
@@ -14,13 +14,14 @@
 # Can specify a specific version on the command line
 if [ -n "$1" ]; then
    LILYPOND_VERSION=$1
-   LILYPOND_BASE=~/oldlilypond/$LILYPOND_VERSION/usr/bin
+   LILYPOND_BASE=/mutopia/lilypond/$LILYPOND_VERSION/usr/bin
 else
    LILYPOND_VERSION=
-   LILYPOND_BASE=~/lilypond/usr/bin
+   #LILYPOND_BASE=/mutopia/lilypond/default/usr/bin
+   LILYPOND_BASE=~/bin
 fi
 
 export LILYPOND_VERSION
 export LILYPOND_BIN=$LILYPOND_BASE/lilypond
 export CONVERT_LY_BIN=$LILYPOND_BASE/convert-ly
-export MUTOPIA_BASE=~/Mutopia
+export MUTOPIA_BASE=/mutopia/MutopiaProject
